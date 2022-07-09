12731d26cd1192ee1220ba376a0f05e4cd133239
Mike Adamski <michael.adamski@abovelending.com>
2022-07-09 13:26:12 -0500

    Get current battery level for android device. TODO: iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,6 +23,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int? _batteryLevel;
+
+  Future<void> _getBatteryLevel() async {
+    const platform = MethodChannel('native.learning.dev/battery');
+    try {
+      final currentLevel = await platform.invokeMethod('getBatteryLevel');
+      setState(() {
+        _batteryLevel = currentLevel;
+      });
+    } on PlatformException catch (e) {
+      setState(() {
+        _batteryLevel = null;
+      });
+    } catch (e) {
+      setState(() {
+        _batteryLevel = null;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getBatteryLevel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Native Code'),
       ),
       body: Center(
-        child: Text('Battery Level: ...'),
+        child: Text('Battery Level: $_batteryLevel'),
       ),
     );
   }
